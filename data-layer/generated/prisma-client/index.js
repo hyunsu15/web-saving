e2db8f44dbd0e8ff8308b33018cc329381f92036
wjdgustn <wjdgustn15@naver.com>
2019-05-27 18:27:32 +0900

    data model복사본

---
+++ data-layer/generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Deal",
+    embedded: false
+  },
+  {
+    name: "Admin",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/wjdgustn15-c9cd71/DBHW/dev`
+});
+exports.prisma = new exports.Prisma();
